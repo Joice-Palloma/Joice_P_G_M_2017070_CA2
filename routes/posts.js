2cd92cd85a87b42e26a0db6698f0769dc358f72a
Joice-Palloma <joicepgm@gmail.com>
2020-04-24 16:54:08 +0000

    patch is working

--- routes/posts.js
+++ routes/posts.js
@@ -24,4 +24,38 @@ try{
 }
 });
 
+//specific posst
+router.get('/:postId', async (req,res) =>{
+    try {
+        const post = await Post.findById(req.params.postId);
+        res.json(post);
+    }catch (err) {
+        res.json({ message: err});
+    }
+    
+});
+
+//delete a specific post
+router.delete('/:postId', async (req,res) => {
+    try{
+        const removePost = await Post.remove({_id:req.params.postId});
+        res.json(removePost);
+    } catch (err) {
+        res.json({ message: err});
+    } 
+});
+
+//update a post
+router.patch('/:postId', async (req,res) => {
+   try{
+        const updatedPost = await Post.updateOne(
+            {_id:req.params.postId},
+            {$set: {title: req.body.title}}
+             );
+        res.json(updatedPost);
+    } catch (err) {
+        res.json({ message: err});
+    }
+    
+});
 module.exports = router;
